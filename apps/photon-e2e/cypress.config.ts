88be5839387d914cc0cd312155f784aa079575e7
adamant-knerd <aashis@sundaygrids.com>
2023-01-27 21:41:24 +0530

    feat(photon-e2e): support report generation

--- apps/photon-e2e/cypress.config.ts
+++ apps/photon-e2e/cypress.config.ts
@@ -2,7 +2,16 @@ import {defineConfig} from 'cypress';
 import {nxE2EPreset} from '@nrwl/cypress/plugins/cypress-preset';
 
 export default defineConfig({
+  reporter: '../../node_modules/cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportDir: '../../dist/cypress/apps/photon-e2e',
+  },
   e2e: {
     ...nxE2EPreset(__dirname),
+    setupNodeEvents(on) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+    experimentalWebKitSupport: true,
   },
 });
--- apps/photon-e2e/src/support/commands.ts
+++ apps/photon-e2e/src/support/commands.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -17,8 +18,37 @@ declare namespace Cypress {
 }
 //
 // -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
+Cypress.Commands.add('login', (username, password) => {
+  console.log(`username: ${username}, password: ${password}`);
+  // cy.visit('/login');
+
+  // cy.get('input[name=username]').type(username);
+
+  // {enter} causes the form to submit
+  // cy.get('input[name=password]').type(`${password}{enter}`, {log: false});
+
+  // we should be redirected to /dashboard
+  // cy.url().should('include', '/dashboard');
+
+  // our auth cookie should be present
+  // cy.getCookie('your-session-cookie').should('exist');
+
+  // UI should reflect this user being logged in
+  // cy.get('h1').should('contain', username);
+
+
+  // with session
+  // cy.session(username, () => {
+  //   cy.visit('/login');
+  //   cy.get('input[name=username]').type(username);
+  //   cy.get('input[name=password]').type(`${password}{enter}`, {log: true});
+  //   cy.url().should('include', '/dashboard');
+  //   cy.get('h1').should('contain', username);
+  // }, {
+  //   validate: () => {
+  //     cy.getCookie('your-session-cookie').should('exist');
+  //   },
+  // });
 });
 //
 // -- This is a child command --
