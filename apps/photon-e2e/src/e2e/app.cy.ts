84adb3172cfbb116cd0f616ac1478c575e06d538
adamant-knerd <aashis@sundaygrids.com>
2023-01-27 15:36:16 +0530

    test(photon-e2e): add tests on external sites

--- apps/photon-e2e/src/e2e/app.cy.ts
+++ apps/photon-e2e/src/e2e/app.cy.ts
@@ -5,9 +5,37 @@ describe('photon', () => {
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
+    // cy.origin('http://localhost:4200', () => {
+    // cy.visit('/');
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains('Welcome photon');
+    // });
+  });
+
+  it('finds the content "type"', () => {
+    cy.origin('https://example.cypress.io', () => {
+      cy.visit('/');
+      cy.contains('type');
+    });
+  });
+
+  it('clicks the link "type"', () => {
+    cy.origin('https://example.cypress.io', () => {
+      cy.visit('/');
+
+      cy.contains('type').click();
+
+      // Should be on a new URL which
+      // includes '/commands/actions'
+      cy.url().should('include', '/commands/actions');
+
+      // Get an input, type into it
+      cy.get('.action-email').type('fake@email.com');
+
+      // Verify that the value has been updated
+      cy.get('.action-email').should('have.value', 'fake@email.com');
+    });
   });
 });
